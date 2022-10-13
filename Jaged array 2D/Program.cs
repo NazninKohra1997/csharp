fdd9429fba008f4c64c61ad75e1b17b63187a9bd
Naznin Kohra Jhora <nkjhora@outlook.com>
2022-10-13 14:33:40 +0600

    added two new files.

---
+++ Jaged array 2D/Program.cs
@@ -0,0 +1,17 @@
+﻿int [][] age = new int [3][];
+
+age[0]=new int [3];
+age[1]=new int [2];
+
+age[0][0]=50;
+age[0][1]=25;
+age[0][2]=22;
+
+age[1][0]=21;
+age[1][1]=16;
+
+Console.WriteLine(age[0][2]);
+Console.WriteLine(age[0][0]);
+Console.WriteLine(age[1][1]);
+Console.WriteLine(age[0][1]);
+
---
+++ jaged array 3D/Program.cs
@@ -0,0 +1,32 @@
+﻿int[][][] arr= new int[2][][];
+
+arr[0] = new int[2][];
+arr[1]=new int[3][];
+
+arr[0][0]=new int[2];
+arr[0][1]=new int[3];
+
+arr[1][0]=new int[3];
+arr[1][1]=new int[2];
+arr[1][2]=new int[1];
+
+arr[0][0][0]=3;
+arr[0][0][1]=2;
+
+arr[0][1][0]=4;
+arr[0][1][1]=2;
+arr[0][1][2]=5;
+
+arr[1][0][0]=2;
+arr[1][0][1]=7;
+arr[1][0][2]=5;
+
+arr[1][1][0]=7;
+arr[1][1][1]=9;
+
+arr[1][2][0]=11;
+
+
+Console.WriteLine(arr[0][0][1]);
+Console.WriteLine(arr[1][0][1]);
+Console.WriteLine(arr[1][2][0]);
