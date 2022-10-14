fb399cf867bd6f0cd0e25cf6648d090b338a53a5
Naznin Kohra Jhora <nkjhora@outlook.com>
2022-10-14 09:40:11 +0600

    I added a new file

---
+++ Multi dimensional array/Program.cs
@@ -0,0 +1,9 @@
+﻿int[,] ages= new int[5,6]{{1,2,3,4,5,6},{7,8,9,10,11,12},{13,14,15,16,17,18},{19,20,21,22,23,24},
+                          {25,26,27,28,29,30}};
+
+Console.WriteLine(ages[0,0]);
+Console.WriteLine(ages[1,0]);
+Console.WriteLine(ages[2,0]);
+Console.WriteLine(ages[3,0]);
+Console.WriteLine(ages[4,0]);
+Console.WriteLine(ages[5,0]);
