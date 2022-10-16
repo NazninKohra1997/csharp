2169b2ec2cd01546b94ced7ba6936bee9cbc514e
Naznin Kohra Jhora <nkjhora@outlook.com>
2022-10-16 20:20:29 +0600

    added files

---
+++ Conditional statement/Program.cs
@@ -0,0 +1,12 @@
+﻿int x=5;
+int y=10;
+
+if(y>5){
+    Console.WriteLine("Y is greater");
+}
+if(x<=5){
+    Console.WriteLine("x is okay");
+}
+else{
+    Console.WriteLine("Nothing is printed");
+}
---
+++ Turnary operator/Program.cs
@@ -0,0 +1,9 @@
+﻿int x=5;
+
+string message= x==5? "X is equal to 5" : "X is not equal to 5";
+Console.WriteLine(message);
+
+
+int y=10;
+string msg= x==10 ? "x is equal to 5" :  y==10 ? "Y equal 10":"Y not equal 10";
+Console.WriteLine(msg);
