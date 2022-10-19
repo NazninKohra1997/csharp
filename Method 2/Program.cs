2c6a58b9314cc4e7b09abf268a31d39ebc285da7
Naznin Kohra Jhora <nkjhora@outlook.com>
2022-10-19 22:13:57 +0600

    I added new files

---
+++ Method 2/Program.cs
@@ -0,0 +1,20 @@
+﻿void IsPrime(int number){
+    if(number<2){
+        Console.WriteLine("Not prime!");
+    }
+    else{
+        for(int i=2;i<=Math.Sqrt(number);i++){
+            if(number%i==0){
+                Console.WriteLine("Not prime!");
+                break;
+            }
+            else{
+                Console.WriteLine("Prime!");
+                break;
+            }
+        }
+    }
+}
+
+int num= int.Parse(Console.ReadLine());
+IsPrime(num);
---
+++ Simple(prime or not prime)/Program.cs
@@ -0,0 +1,23 @@
+﻿//taking input
+int number = int.Parse(Console.ReadLine()); //converting string to int
+
+//checking prime or not
+if(number<2){
+    Console.WriteLine("Not prime!");
+}
+
+
+else{
+    for(int i=2;i<=Math.Sqrt(number);i++){
+        if(number%i==0){
+            Console.WriteLine("Not prime!");
+           break;
+        }
+       else{
+            Console.WriteLine("Prime!");
+            break;
+        }
+    }
+}
+
+//Console.WriteLine(Math.Sqrt(2));
