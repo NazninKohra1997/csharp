cbccbb62354ffd562baf30c3e574f3eaf0ba9519
Naznin Kohra Jhora <nkjhora@outlook.com>
2022-10-19 23:50:30 +0600

    I added a new file

---
+++ String and character method/Program.cs
@@ -0,0 +1,13 @@
+﻿//string method
+
+string name= "Naznin";
+name=name.Replace('a','n'); //method 
+Console.WriteLine(name);
+
+int num=name.IndexOf('i');
+Console.WriteLine(num); //another method
+
+
+//character method
+bool a=char.IsPunctuation(',');
+Console.WriteLine(a);
