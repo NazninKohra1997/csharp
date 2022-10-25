3281b79000e497adb5ede80c06b1ddb4b0b7ba85
Naznin Kohra Jhora <nkjhora@outlook.com>
2022-10-25 10:44:55 +0600

    I added a new file

---
+++ Array initialization into single line/Program.cs
@@ -0,0 +1,18 @@
+﻿int number=int.Parse(Console.ReadLine());
+var arr= new int[number,number];
+
+for(var i=0;i<number;i++){
+    var num=new string[number];
+    num=Console.ReadLine().Split();
+    for(var j=0; j<number;j++){
+        
+        arr[i,j]=int.Parse(num[j]);
+    }
+}
+
+for(int i=0;i<number;i++){
+    for(int j=0;j<number;j++){
+    Console.Write(arr[i,j]);
+    Console.Write(" ");
+    }
+}
