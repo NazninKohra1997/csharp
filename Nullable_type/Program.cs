d9c2551391457093ec652b3ef10f70ca6922b50d
Naznin Kohra Jhora <nkjhora@outlook.com>
2022-11-22 20:15:34 +0600

    I added a new file

---
+++ Nullable_type/Program.cs
@@ -0,0 +1,34 @@
+﻿using oop;
+
+int? x=null;
+
+x=5;
+
+if(x.HasValue){
+    Console.WriteLine(x.Value);
+}
+else{
+    Console.WriteLine("null");
+}
+
+
+
+
+Person p= getName("Raisa");
+
+Person getName(string Name){
+    if(Name=="Raisa"){
+        return new Person();
+    }
+    else{
+        return null;
+    }
+
+
+}
+
+p?.Talk("Hello");
+
+int id=p?.id ?? 0;
+Console.WriteLine(id);
+
---
+++ Nullable_type/class1.cs
@@ -0,0 +1,8 @@
+namespace oop;
+
+public class Person{
+    public int id=101;
+    public void Talk(string name){
+        Console.WriteLine(name);
+    }
+}
