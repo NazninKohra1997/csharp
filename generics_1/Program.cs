365516431080bcde772076d4c247422fe72a1021
Naznin Kohra Jhora <nkjhora@outlook.com>
2023-01-27 14:01:25 +0600

    I added a new file

---
+++ generics_1/Program.cs
@@ -0,0 +1,9 @@
+﻿using generics;
+
+CoOrdinate<int> co =new CoOrdinate<int>();
+
+co.X=200;
+co.Y=100;
+
+Console.WriteLine(co.X);
+Console.WriteLine(co.Y);
---
+++ generics_1/generics.cs
@@ -0,0 +1,8 @@
+namespace generics
+{
+    public class CoOrdinate<T>{
+        public T  X{get;set;}
+        public T  Y{get;set;}
+    }
+    
+}
