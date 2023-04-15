60c1f22ef99d47d19a813143be24e70f130b4467
NazninKohra1997 <jhora.eceiu@gmail.com>
2023-04-15 13:03:26 -0700

    I added some files

---
+++ C# codes/Queue/Program.cs
@@ -0,0 +1,21 @@
+﻿Queue<string> q = new Queue<string>();   //first in first out
+
+q.Enqueue("Water");
+q.Enqueue("CocaCola");
+q.Enqueue("Pepsi");
+q.Enqueue("Sprite");
+
+foreach(var i in q)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine();
+
+q.Dequeue();
+q.Dequeue();
+
+foreach(var i in q)
+{
+    Console.WriteLine(i);
+}
---
+++ C# codes/Stack/Program.cs
@@ -0,0 +1,29 @@
+﻿Stack<string> s = new Stack<string>();
+
+s.Push("Apple");
+s.Push("Mango");
+s.Push("Banana");
+s.Push("Orange");
+
+foreach(var i  in s)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine();
+
+s.Pop();
+s.Pop();
+
+foreach (var i in s)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine();
+
+s.Peek();
+foreach (var i in s)
+{
+    Console.WriteLine(i);
+}
