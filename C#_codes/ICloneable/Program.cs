2f31332268a0900171b7533fed9b2cb195d2565c
NazninKohra1997 <jhora.eceiu@gmail.com>
2023-04-17 12:33:49 -0700

    I added a new file

---
+++ C#_codes/ICloneable/Program.cs
@@ -0,0 +1,16 @@
+﻿using ImportantInterface;
+
+Product product = new Product();
+
+product.Name = "Camera";
+product.Price = 3000;
+
+
+if (product is ICloneable)
+{
+    Product product1 = (Product)product.Clone();
+    Console.WriteLine(product1.Name);
+    Console.WriteLine(product1.Price);
+
+}
+
