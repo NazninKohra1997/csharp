cea49551abf133c67d083e7b59979a3f538a1fb6
NazninKohra1997 <jhora.eceiu@gmail.com>
2023-07-16 22:30:34 +0600

    I added two new files

---
+++ C#_codes/Annonymous_method/Program.cs
@@ -0,0 +1,10 @@
+﻿/*int sum(int a,int b)
+{
+    return a + b;
+}*/
+
+
+Func<int, int, int> something = (a,b) => a+b;
+
+int sum = something(5, 6);
+Console.WriteLine(sum);
---
+++ C#_codes/Lambda_method/Program.cs
@@ -0,0 +1,10 @@
+﻿/*int Sum(int a,int b)
+{
+    return a + b;
+}*/
+
+
+int Sum(int a, int b) => a + b;
+
+int result = Sum(5, 3);
+Console.WriteLine(result);
