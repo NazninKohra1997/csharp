5f27d9396f9f3d29643eb691e7752963262310cd
NazninKohra1997 <jhora.eceiu@gmail.com>
2023-07-17 14:37:30 +0600

    I added a new file

---
+++ C#_codes/Event_01/Printer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event_01
+{
+   
+     public class Printer
+    {
+        public delegate void DoSomething(string a);
+        public event DoSomething something;
+
+        public void Printed()
+        {
+            if(something != null)
+            {
+                something.Invoke("The work is done!");
+            }
+        }
+    }
+
+ }
+
---
+++ C#_codes/Event_01/Program.cs
@@ -0,0 +1,7 @@
+﻿using Event_01;
+
+Printer printer = new Printer();
+printer.something += (a) => Console.WriteLine(a);    //annonymous method
+
+
+printer.Printed();
