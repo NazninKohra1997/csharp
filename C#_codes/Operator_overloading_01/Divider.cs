cc67a9c5cc7d4dbd386711cccf7af4df443ecd96
NazninKohra1997 <jhora.eceiu@gmail.com>
2023-07-21 15:11:46 +0600

    I added some files

---
+++ C#_codes/Operator_overloading_01/Divider.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator_overloading_01
+{
+    public class Divider
+    {
+        private readonly int Num;
+
+        private readonly int Den;
+
+        public Divider(int Num, int Den)
+        {
+            this.Num = Num;
+            this.Den = Den;
+        }
+
+        public int Divide1
+        {
+            get
+            {
+                return Num;
+            }
+        }
+
+        public int Divide2
+        {
+            get
+            {
+                return Den;
+            }
+        }
+
+        public static Divider operator /(Divider d1,Divider d2)
+        {
+            return new Divider(d1.Num*d2.Den , d1.Den*d2.Num);
+            
+
+    }
+       
+
+    }
+}
---
+++ C#_codes/Operator_overloading_01/Program.cs
@@ -0,0 +1,21 @@
+﻿using Operator_overloading_01;
+
+//Divider d = new Divider(4,2);
+
+
+var d1 = new Divider(4,2);
+var d2 = new Divider(8, 10);
+
+
+var t = d1 / d2;
+
+Console.WriteLine($"{t.Divide1}/{t.Divide2}");
+
+
+
+
+
+
+
+
+//Console.WriteLine($"{d3.Num}/{Den}")
