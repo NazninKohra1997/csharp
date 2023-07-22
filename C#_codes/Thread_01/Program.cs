7ef0721789016a1b029f546c0e04b9100116955d
NazninKohra1997 <jhora.eceiu@gmail.com>
2023-07-22 23:28:50 +0600

    I added some files

---
+++ C#_codes/Thread_01/Program.cs
@@ -0,0 +1,28 @@
+﻿Thread t1 = new Thread(PrintEven);
+Thread t2 = new Thread(PrintOdd);
+
+t1.Start();
+t2.Start();
+//PrintOdd();
+
+
+//PrintEven();
+//Console.WriteLine();
+//PrintOdd();         Doesn't work parallely
+
+
+void PrintEven()
+{
+    for(int i=0; i<=20; i += 2)
+    {
+        Console.WriteLine(i);
+    }
+}
+
+void PrintOdd()
+{
+    for(int i=1 ;i<=19; i+= 2)
+    {
+        Console.WriteLine(i);
+    }
+}
---
+++ C#_codes/Thread_02/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+Thread t1 = new Thread(() => Even(200));
+Thread t2 = new Thread(() => Odd(200));
+
+t1.Start();
+t2.Start();
+
+
+
+
+void Even(int item)
+{
+    for(int i=0; i<=item; i += 2)
+    {
+        Console.WriteLine(i);
+    }
+}
+
+
+void Odd(int item)
+{
+    for (int i = 1; i <= item; i += 2)
+    {
+        Console.WriteLine(i);
+    }
+}
