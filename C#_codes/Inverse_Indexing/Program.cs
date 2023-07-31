2c1aee49ef4ae26c65cfa90c0dd9de05be656db5
NazninKohra1997 <jhora.eceiu@gmail.com>
2023-07-31 16:07:18 +0600

    I added a new file

---
+++ C#_codes/Inverse_Indexing/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] ages = new int[6] { 5, 98, 22, 19, 2, 7 };
+
+for(int i = 0; i < 6; i++)
+{
+    Console.WriteLine(ages[i]);
+}
+
+Console.WriteLine();
+
+
+Console.WriteLine(ages[^1]);
+Console.WriteLine(ages[^2]);
+Console.WriteLine(ages[^3]);
+Console.WriteLine(ages[^4]);
+Console.WriteLine(ages[^5]);
+Console.WriteLine(ages[^6]);
