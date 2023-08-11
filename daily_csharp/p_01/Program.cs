7b3852b61a6b5f8e42785d433c90a8e9baf07e6d
NazninKohra1997 <jhora.eceiu@gmail.com>
2023-08-11 14:23:09 +0600

    I added some files

---
+++ daily_csharp/p_01/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] ages = new int[5];
+
+for(int i=0; i<ages.Length; i++)
+{
+    ages[i] = int.Parse(Console.ReadLine());
+}
+
+
+Console.WriteLine();
+
+foreach(var age in ages)
+{
+    Console.WriteLine(age);
+}
---
+++ daily_csharp/p_02/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] names = new string[4];
+
+for(int i=0;i<names.Length; i++)
+{
+    names[i] = Console.ReadLine();
+}
+
+Console.WriteLine();
+
+foreach(var name in names)
+{
+    Console.WriteLine(name);
+}
