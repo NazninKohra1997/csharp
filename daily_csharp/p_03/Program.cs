ed7a28246b7bd39472626c52d089b0b02f398538
NazninKohra1997 <jhora.eceiu@gmail.com>
2023-08-12 14:32:32 +0600

    I added some files

---
+++ daily_csharp/p_03/Program.cs
@@ -0,0 +1,29 @@
+﻿int[,] ages = new int[2, 3];
+
+for(int i=0; i<2; i++)
+{
+    for(int j=0; j < 3; j++)
+    {
+        ages[i, j] = int.Parse(Console.ReadLine());
+    }
+}
+
+
+Console.WriteLine();
+
+for (int i = 0; i < 2; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        Console.Write(ages[i, j]);
+        if (i == 1 && j == 2)
+        {
+            Console.Write("");
+        }
+        else
+        {
+            Console.Write(",");
+        }
+    }
+    Console.WriteLine();
+}
---
+++ daily_csharp/p_o4/Program.cs
@@ -0,0 +1,30 @@
+﻿string[,] names = new string[3, 2];
+
+
+for(int i=0; i<3; i++)
+{
+    for(int j=0;j<2; j++)
+    {
+        names[i,j] = Console.ReadLine();
+    }
+}
+
+
+Console.WriteLine();
+
+for(int i=0; i < 3; i++)
+{
+    for (int j=0; j<2; j++)
+    {
+        Console.Write(names[i,j]);
+        if (i == 2 && j == 1)
+        {
+            Console.Write("");
+        }
+        else
+        {
+            Console.Write(",");
+        }
+    }
+    Console.WriteLine();
+}
