7648c1ff795e1b63116c0007f3c22f6ba6873347
NazninKohra1997 <jhora.eceiu@gmail.com>
2023-08-13 14:02:00 +0600

    I added two files

---
+++ daily_csharp/p_05/Program.cs
@@ -0,0 +1,18 @@
+﻿int[][] arr = new int[2][];
+
+
+arr[0] = new int[2];
+arr[1] = new int[3];
+
+
+arr[0][0] = 30;
+arr[0][1] = 33;
+
+arr[1][0] = 40;
+arr[1][1] = 44;
+arr[1][2] = 49;
+
+
+
+Console.WriteLine(arr[1][1]);
+
---
+++ daily_csharp/p_06/Program.cs
@@ -0,0 +1,26 @@
+﻿int[][][] age = new int[2][][];
+
+age[0] = new int[2][];
+age[1] = new int[1][];
+
+
+
+age[0][0] = new int[2];
+age[0][1] = new int[1];
+
+age[1][0] = new int[2];
+
+
+
+
+age[0][0][0] = 20;
+
+age[0][1][0] = 30;
+
+age[1][0][0] = 40;
+age[1][0][1] = 42;
+
+
+
+
+Console.WriteLine(age[1][0][0]);
