df31f44de7e7fead642eabd63ed8cc48e7131a2d
NazninKohra1997 <jhora.eceiu@gmail.com>
2023-08-14 10:13:51 +0600

    I modified and added some files

---
+++ daily_csharp/p_07/Program.cs
@@ -0,0 +1,27 @@
+﻿int[][][] roll = new int[2][][];
+
+roll[0] = new int[2][];
+roll[1] = new int[1][];
+
+
+
+roll[0][0] = new int[2];
+roll[0][1] = new int[1];
+
+roll[1][0] = new int[1];
+
+
+
+
+roll[0][0][0] = 101;
+roll[0][0][1] = 102;
+
+roll[0][1][0] = 103;
+
+
+roll[1][0][0] = 202;
+
+
+
+
+Console.WriteLine(roll[1][0][0]);
---
+++ daily_csharp/p_08/Program.cs
@@ -0,0 +1,45 @@
+﻿int[] arr = new int[5] { 55, 45, 99, 76, 101 };
+
+
+for(int i=0; i<arr.Length; i++)
+{
+    Console.WriteLine(arr[i]);
+}
+
+
+Console.WriteLine();
+
+
+for(int i=0; i<arr.Length ; i++)
+{
+    while (i < 5)
+    {
+        int a = arr[i] + 1;
+        Console.WriteLine(a);
+        break;
+    }
+}
+
+Console.WriteLine();
+
+
+for(int i=0;i<arr.Length ; i++)
+{
+   
+    do{
+       int j = arr[i] + 2;
+        Console.WriteLine(j);
+        break;
+    } while (i < 5);
+   
+  
+
+}
+
+
+Console.WriteLine();
+
+foreach(var a in arr)
+{
+    Console.WriteLine(a);
+}
