e6024088826ad9e9c0ae92afb57b17656fb26635
NazninKohra1997 <jhora.eceiu@gmail.com>
2023-08-16 10:58:55 +0600

    I added two files

---
+++ daily_csharp/p_14/Electronics.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p_14
+{
+   public class Electronics
+    {
+        public string name;
+        public int number;
+
+
+        public void GetElectronics()
+        {
+            Console.WriteLine(name);
+            Console.WriteLine(number);
+        }
+    }
+}
---
+++ daily_csharp/p_14/Program.cs
@@ -0,0 +1,7 @@
+﻿using p_14;
+
+Electronics electronics = new Electronics();
+electronics.name = "Laptop";
+electronics.number = 20;
+
+electronics.GetElectronics();
