27f44c5374b8385e8ea9b30743777cd4c2fbf540
NazninKohra1997 <jhora.eceiu@gmail.com>
2023-08-19 20:54:42 +0600

    I added a new file

---
+++ daily_csharp/p_17/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p_17
+{
+   public class Employee
+    {
+        private string Name { get; set; }
+        private string Department { get; set; }
+        private int Age { get; set;}
+
+
+        public string NameOfEmployee
+        {
+            set
+            {
+                Name = value;
+            }
+        }
+
+        public string DepartmentOfEmployee
+        {
+            set { 
+            Department = value;
+            }
+        }
+
+        public int AgeOfEmployee
+        {
+            set
+            {
+                Age = value;
+            }
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine(Name);
+            Console.WriteLine(Department);
+            Console.WriteLine(Age);
+        }
+    }
+}
---
+++ daily_csharp/p_17/Program.cs
@@ -0,0 +1,9 @@
+﻿using p_17;
+
+Employee employee = new Employee();
+
+employee.NameOfEmployee = "Jhora";
+employee.AgeOfEmployee = 26;
+employee.DepartmentOfEmployee = "Software Engineering";
+
+employee.GetInfo();
