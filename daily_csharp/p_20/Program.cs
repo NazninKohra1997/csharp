e7fc9ba006bdab5811266c39ae46c5927ad9c5b6
NazninKohra1997 <jhora.eceiu@gmail.com>
2023-09-20 11:23:54 +0600

    I added a new file

---
+++ daily_csharp/p_20/Program.cs
@@ -0,0 +1,7 @@
+﻿using p_20;
+
+Student student = new Student(01, "Raina", 10);
+
+student.GetInfo();
+
+
---
+++ daily_csharp/p_20/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p_20
+{
+   public class Student
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        public int Roll { get; private set; }
+
+
+        public Student(int Id,  string Name, int Roll)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Roll = Roll;
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine(Id);
+            Console.WriteLine(Name);
+            Console.WriteLine(Roll);
+        }
+
+        ~Student()
+        {
+
+        }
+       
+    }
+}
