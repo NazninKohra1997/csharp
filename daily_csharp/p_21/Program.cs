46413531e528bafbf1ab844b525a98d5bcedca9f
NazninKohra1997 <jhora.eceiu@gmail.com>
2023-09-24 13:00:16 +0600

    I added a new project

---
+++ daily_csharp/p_21/Program.cs
@@ -0,0 +1,14 @@
+﻿using p_21;
+
+WaterBottle waterBottle = new WaterBottle(2, "Pran", 50);
+
+
+double total1 = waterBottle.WaterAmount(50);
+double total2  = waterBottle.WaterAmount(25.5);
+
+double totalWater = total1 + total2;
+
+waterBottle.GetInfo();
+Console.Write("Total water amount of the bottle is ");
+Console.Write(totalWater);
+Console.WriteLine(" ml");
---
+++ daily_csharp/p_21/WaterBottle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p_21
+{
+    public class WaterBottle
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public int Amount { get; private set; }
+
+
+        public WaterBottle(int Id,  string Name, int Amount)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Amount = Amount;
+        }
+
+        public double WaterAmount(int amount)
+        {
+            double totalAmount = Amount + amount;
+            return totalAmount;
+        }
+
+        public double WaterAmount(double amount)
+        {
+
+            return amount;
+        }
+
+        public void GetInfo()
+        {
+            Console.Write("Id is ");
+            Console.WriteLine(Id);
+            Console.Write("Name is ");
+            Console.WriteLine(Name);
+            Console.Write("Amount is ");
+            Console.WriteLine(Amount);
+        }
+    }
+}
